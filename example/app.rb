109574987570f86c752ebbe535dbe393abbb134c
Christian <zarski@gmail.com>
2016-07-29 08:07:41 -0400

    Receive a webhook post in example app

--- example/app.rb
+++ example/app.rb
@@ -22,8 +22,8 @@ end
 
 helpers do
   def verify_webhook(data, hmac_header)
-    digest  = OpenSSL::Digest.new('sha256')    
-    calculated_hmac = Base64.encode64(OpenSSL::HMAC.digest(digest, VERIFIER_TOKEN, data)).strip    
+    digest  = OpenSSL::Digest.new('sha256')
+    calculated_hmac = Base64.encode64(OpenSSL::HMAC.digest(digest, VERIFIER_TOKEN, data)).strip
     calculated_hmac == hmac_header
   end
 end
