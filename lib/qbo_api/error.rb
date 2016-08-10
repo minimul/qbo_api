d17e69b7b3c5d7570bfceaa7f91fbfed8b8b9e19
Christian <zarski@gmail.com>
2016-08-10 05:56:11 -0400

    Handle HTTP Status code 429 -> Too Many Requests. Spec it when it is live

--- lib/qbo_api/error.rb
+++ lib/qbo_api/error.rb
@@ -4,7 +4,8 @@
 #401 Unauthorized	Authentication or authorization has failed.
 #403 Forbidden	The resource is forbidden.
 #404 Not Found	The resource is not found.
-#500 Internal Server Error	An error occured on the server while processing the request.  Resubmit request once; if it persists, contact developer support.
+#429 Too Many Requests  API Throttling/ Rate limiting
+#500 Internal Server Error	An error occurred on the server while processing the request.  Resubmit request once; if it persists, contact developer support.
 #503 Service Unavailable	The service is temporarily unavailable.
 # Custom error class for rescuing from all QuickBooks Online errors
 class QboApi
@@ -32,6 +33,9 @@ class QboApi
 
   # Raised when QuickBooks Online returns the HTTP status code 404
   class NotFound < Error; end
+
+  # Raised when QuickBooks Online returns the HTTP status code 429
+  class TooManyRequests < Error; end
 
   # Raised when QuickBooks Online returns the HTTP status code 500
   class InternalServerError < Error; end
