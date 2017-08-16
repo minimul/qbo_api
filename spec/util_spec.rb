36114701f3607f60238c53bfb0e7eabb41da8fcb
Aldis Berjoza <aldis@berjoza.lv>
2017-08-16 15:51:13 +0300

    Fixed test for time zones with positive offset

--- spec/util_spec.rb
+++ spec/util_spec.rb
@@ -9,7 +9,7 @@ describe QboApi::Util do
 
   it 'convert Time objects to proper CDC "changed since" time stamp' do
     api = QboApi.new(creds.to_h)
-    expect(api.cdc_time(Time.now)).to match /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}-\d{2}:\d{2}$/
+    expect(api.cdc_time(Time.now)).to match /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}[+-]\d{2}:\d{2}$/
   end
 
   describe '.esc' do
